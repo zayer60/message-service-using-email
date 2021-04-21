1772e621d76a78a384f6d01a4dbd73f299c66cfb
zayer60 <zayerwali12@gmail.com>
2021-04-21 14:17:26 +0530

    making a little responsive

--- sendemailapp/urls.py
+++ sendemailapp/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path('article/', PatientListView.as_view(), name='patient-list'),
-    path('article/create/', CreatePatient.as_view(), name='create-patient'),
-    path('article/<int:pk>/edit/', UpdatePatient.as_view(), name='update-patient'),
-    path('article/<int:pk>/delete', DeletePatient.as_view(), name='delete-patient'),
-    path('group/',GroupListView.as_view(),name='group-list'),
-    path('group/<int:id>',groupdetail,name='group-details'),
-    path('group/<int:pk>/edit',UpdateGroup.as_view(),name='group-update'),
-    path('group/create',CreateGroup.as_view(),name='group-create'),
-    path('group/<int:pk>/delete',DeleteGroup.as_view(),name='group-delete'),
+    path('',GroupListView.as_view(),name='group-list'),
+    path('<int:id>/',groupdetail,name='group-details'),
+    path('<int:pk>/edit/',UpdateGroup.as_view(),name='group-update'),
+    path('create/',CreateGroup.as_view(),name='group-create'),
+    path('<int:pk>/delete/',DeleteGroup.as_view(),name='group-delete'),
+    path('patient/', PatientListView.as_view(), name='patient-list'),
+    path('patient/create/', CreatePatient.as_view(), name='create-patient'),
+    path('patient/<int:pk>/edit/', UpdatePatient.as_view(), name='update-patient'),
+    path('patient/<int:pk>/delete', DeletePatient.as_view(), name='delete-patient'),
+
 ]
